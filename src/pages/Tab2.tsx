f34aad78175bc242195d63e46e51a617d7789d48
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-06-14 23:03:57 -0500

    resizing/optimizing photos, added ion-slides on tab2

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,51 +1,218 @@
-import 
-{ 
-  IonContent, 
-  IonHeader, 
-  IonPage, 
-  IonTitle, 
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
   IonToolbar,
   IonItem,
   IonCard,
   IonImg,
   IonIcon,
   IonLabel,
-  IonCardTitle, 
-} 
-from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+  IonCardTitle,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption,
+  IonSlides,
+  IonSlide,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+} from "@ionic/react";
+
+import { ellipsisHorizontal, ellipsisVertical, archive } from "ionicons/icons";
+
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab2.css";
+
+const slideOpts = {
+  initialSlide: 1,
+  speed: 400,
+};
 
 const Tab2: React.FC = () => {
+
   return (
     <IonPage>
-      <IonContent fullscreen>
+      <IonContent fullscreen color="medium">
         <IonCard color="primary" className="ion-margin">
+          <IonSlides pager={true} options={slideOpts}>
+            <IonSlide>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary">
+                      <IonImg
+                        className="slidingpic"
+                        src={`/assets/opt-pic1.png`}
+                      />
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary" className="ion-text-center">
+                      <IonLabel>Electrical Work</IonLabel>
+                    </IonItem>
 
+                    <IonItem color="primary" className="ion-no-border">
+                      <IonText className="ion-text-center ion-no-border">
+                        An intricate electrical installation on the ceiling of
+                        an industrial building.
+                      </IonText>
+                    </IonItem>
+                    <IonItem className="ion-text-center" color="primary"></IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonSlide>
 
+            <IonSlide>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary">
+                      <IonImg
+                        className="slidingpic"
+                        src={`/assets/opt-pic2.png`}
+                      />
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary" className="ion-text-center">
+                      <IonLabel>Installing Electrical Outlets</IonLabel>
+                    </IonItem>
+                    <IonItem color="primary" className="no-border">
+                      <IonText>
+                        An electrical outlet installation in a residental home.
+                      </IonText>
+                    </IonItem>
+                    <IonItem className="no-border" color="primary"></IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonSlide>
 
+            <IonSlide>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary">
+                      <IonImg
+                        className="slidingpic"
+                        src={`/assets/opt-pic3.png`}
+                      />
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary" className="ion-text-center">
+                      <IonLabel>Drywall Installation</IonLabel>
+                    </IonItem>
+                    <IonItem color="primary" className="no-border">
+                      <IonText>A drywall installation for a garage.</IonText>
+                    </IonItem>
+                    <IonItem className="no-border" color="primary"></IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonSlide>
 
-          <IonItem color="primary"></IonItem>
+            <IonSlide>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary">
+                      <IonImg
+                        className="slidingpic"
+                        src={`/assets/opt-pic4.png`}
+                      />
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary" className="ion-text-center">
+                      <IonLabel>Light Installation</IonLabel>
+                    </IonItem>
+                    <IonItem color="primary" className="no-border">
+                      <IonText>
+                        A lighting installation job in a industrial building.
+                      </IonText>
+                    </IonItem>
+                    <IonItem className="no-border" color="primary"></IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonSlide>
 
+            <IonSlide>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary">
+                      <IonImg
+                        className="slidingpic"
+                        src={`/assets/opt-pic5.png`}
+                      />
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary" className="ion-text-center">
+                      <IonLabel>Light Installation</IonLabel>
+                    </IonItem>
+                    <IonItem color="primary" className="no-border">
+                      <IonText>
+                        Another lighting installation job in a industrial
+                        building.
+                      </IonText>
+                    </IonItem>
+                    <IonItem className="no-border" color="primary"></IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonSlide>
 
-          <IonItem color="primary" className="ion-activated">
-            <IonImg 
-            src="/assets/pic1.jpg" 
-            />
-          </IonItem>
-
-
-
-          <IonItem color="primary">
-            <IonLabel > (512) 359-2470</IonLabel>
-          </IonItem>
-
-
-
+            <IonSlide>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary">
+                      <IonImg
+                        className="slidingpic"
+                        src={`/assets/opt-pic6.png`}
+                      />
+                    </IonItem>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonItem color="primary" className="ion-text-center">
+                      <IonLabel>Drywall Installation/Painting</IonLabel>
+                    </IonItem>
+                    <IonItem color="primary" className="no-border">
+                      <IonText>
+                        Drywall installation job followed by paint and
+                        texturing.
+                      </IonText>
+                    </IonItem>
+                    <IonItem className="no-border" color="primary"></IonItem>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonSlide>
+          </IonSlides>
         </IonCard>
       </IonContent>
     </IonPage>
   );
 };
-
 export default Tab2;
